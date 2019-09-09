9e5549a401214a59217147cbc65fd10c3729cb54
tongkevn <tongkevn@umich.edu>
2019-09-09 19:40:06 -0400

    Imported only randint

--- helloworld.py
+++ helloworld.py
@@ -1,10 +1,11 @@
 from random import randint
 
-print("Kevin Tong")
 a = randint(1, 100)
 b = randint(1, 100)
 sum = a + b
 avg = sum / 2.0
+
+print("Kevin")
 print(a)
 print(b)
 print("Sum = " + str(sum))
