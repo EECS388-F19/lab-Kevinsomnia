57e2b3df5641265b51e4172ca93a1280937b46c0
tongkevn <tongkevn@umich.edu>
2019-09-09 19:33:08 -0400

    Python file

---
+++ helloworld.py
@@ -0,0 +1,11 @@
+import random
+
+print("Kevin Tong")
+a = random.randint(1, 100)
+b = random.randint(1, 100);
+sum = a + b
+avg = sum / 2.0
+print(a)
+print(b)
+print("Sum = " + str(sum))
+print("Average = " + str(avg))
